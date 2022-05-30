e7525621e3fcfaa3b375961246874562ce237302
PoziTV <olkhovskij-dima@mail.ru>
2022-05-30 23:14:14 +0600

    map 001

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,32 @@
 package homework
 
+import (
+	"fmt"
+	"sort"
+)
+
+type mapIntStr struct {
+	key int
+	val string
+}
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	sliceToSort := []mapIntStr{}
+
+	for key, val := range input {
+		sliceToSort = append(sliceToSort, mapIntStr{key, val})
+	}
+
+	fmt.Println(sliceToSort)
+
+	sort.Slice(sliceToSort, func(i, j int) bool {
+		return sliceToSort[i].key < sliceToSort[j].key
+	})
+
+	fmt.Println(sliceToSort)
+	for _, val := range sliceToSort {
+		result = append(result, val.val)
+	}
+
 	return
 }
